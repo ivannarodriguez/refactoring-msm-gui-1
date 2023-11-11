6a8e56a2813cf22904d3e43fdd510023a578abc4
Ivanna <44304361+ivannarodriguez@users.noreply.github.com>
2023-11-11 05:53:45 +0000

    almost

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -11,4 +11,9 @@
 #  updated_at :datetime         not null
 #
 class Actor < ApplicationRecord
+  def characters
+    my_id = self.id
+    matching_characters = Character.where({ :id => my_id })
+    return matching_characters
+  end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -10,4 +10,10 @@
 #  movie_id   :integer
 #
 class Character < ApplicationRecord
+  def movie
+    my_movie_id = self.movie_id
+    matching_movies = Movie.where({ :id => my_movie_id })
+    the_movie = matching_movies[0]
+    return the_movie
+  end
 end
